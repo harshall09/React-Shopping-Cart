d9f86400b751732df9d0f842ebcb4923221855e2
harshall09 <harshal.shah@technostacks.com>
2024-05-02 17:33:34 +0530

    initial commit

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -7,17 +7,20 @@ const AboutPage: React.FC = () => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="container mx-auto py-8 flex-grow">
-        <div className="max-w-screen-lg mx-auto" style={{ width: "90%", margin: "auto" }}>
+        <div
+          className="max-w-screen-lg mx-auto"
+          style={{ width: "90%", margin: "auto" }}
+        >
           <h1 className="text-3xl font-semibold mb-4">About Us</h1>
           <p className="text-lg mb-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
             tristique fringilla nunc, in fringilla ante accumsan vel. Nulla
             facilisi. Sed vitae orci tortor. Sed eu nulla nec ex efficitur
-            bibendum. Phasellus vel varius nunc, sit amet convallis dolor.
-            Donec ultricies odio a justo feugiat, at aliquet sem suscipit.
-            Fusce at neque ac leo sollicitudin sagittis. Nullam lobortis,
-            sapien non cursus malesuada, magna nunc maximus elit, ac ultrices
-            purus tortor at risus.
+            bibendum. Phasellus vel varius nunc, sit amet convallis dolor. Donec
+            ultricies odio a justo feugiat, at aliquet sem suscipit. Fusce at
+            neque ac leo sollicitudin sagittis. Nullam lobortis, sapien non
+            cursus malesuada, magna nunc maximus elit, ac ultrices purus tortor
+            at risus.
           </p>
           <p className="text-lg">
             Etiam auctor ligula sed purus convallis, sed vehicula tortor
