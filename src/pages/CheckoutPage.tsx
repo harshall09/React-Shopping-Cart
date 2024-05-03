5e9c926463707c67f96a52ffc2b2e23c3671f1d4
harshall09 <harshal.shah@technostacks.com>
2024-05-03 10:27:46 +0530

    initial commit

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -6,7 +6,10 @@ const CheckoutPage: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <div className="container mx-auto py-8 flex-grow">
+      <div
+        className="container mx-auto py-8 flex-grow"
+        style={{ width: "90%", margin: "auto" }}
+      >
         <h1 className="font-bold text-3xl mb-4">Checkout</h1>
         <form className="mx-auto max-w-lg">
           <div className="grid grid-cols-2 gap-4 mb-4">
